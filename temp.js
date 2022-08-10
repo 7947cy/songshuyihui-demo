d1bf9b204e5e2d4f6b650409aa05ebd69ce8b8b9
Cy <ensiezadi@gmail.com>
2022-08-10 15:09:13 +0800

    add a license

---
+++ temp.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Just a try
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new to the world!
+// @author       Cy
+// @match        https://www.bilibili.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @license      GPL-3.0-only
+// ==/UserScript==
+ 
+(
+    function() {
+    'use strict';       //使用严格模式
+ 
+    // Your code here...
+    console.log("hello,world");     //打印hello,world
+}
+)();
